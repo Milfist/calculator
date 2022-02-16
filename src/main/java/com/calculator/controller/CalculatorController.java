1deca8733a479fb6b286656ef435764486ad7f3b
Miguel Anguita <milfist@gmail.com>
2022-02-16 11:32:43 +0100

    feat: Controller + Test OK

--- src/main/java/com/calculator/controller/CalculatorController.java
+++ src/main/java/com/calculator/controller/CalculatorController.java
@@ -1,12 +1,21 @@
 package com.calculator.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.calculator.domain.CalculatorResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping(value = "api/v1/servicePointBalances/{id}")
+@RequestMapping(value = "api/v1")
 public class CalculatorController {
 
+  @GetMapping("/sum")
+  public ResponseEntity<CalculatorResponse> sum(@RequestParam(value = "num1") Integer num1, @RequestParam(value = "num2") Integer num2) {
+
+    Integer sum = num1 + num2;
+
+    return new ResponseEntity<>(new CalculatorResponse(String.valueOf(sum)), HttpStatus.OK);
+  }
 
 
 
